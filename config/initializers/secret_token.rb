81cebe3a47da4efa832ac2ac4667f1d305534e9a
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2013-01-25 22:27:24 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Knowledgebase::Application.config.secret_token = '6afa6f851e513aa9ed2f258a8bd3f726ab7fcc8f7dd47330128a5e32a0c6290797c884f562a8a2c1811df341617eaecf16c772cf4d162f631213457c222b388e'
