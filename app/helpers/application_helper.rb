7081782ddee42f857c3a2f23e23939e184df103b
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2013-02-05 20:54:36 +0200

    refactor skill's menu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,11 @@ module ApplicationHelper
   def current_page_profile?
     current_page?(edit_user_registration_path) || current_page?(profile_users_path)
   end
+
+  def menu_item(title, path)
+    css_class = "active" if current_page? path
+    content_tag :li, class: css_class do
+      link_to title, path
+    end
+  end
 end
